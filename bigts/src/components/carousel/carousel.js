ab6f3a239d1e52bc59615c14ca7f65ba818d2593
Matthew <matttali09@yahoo.com>
2022-07-06 17:34:59 -0500

    adjusting images

--- bigts/src/components/carousel/carousel.js
+++ bigts/src/components/carousel/carousel.js
@@ -4,9 +4,9 @@ import AwsSliderStyles from 'react-awesome-slider/src/core/styles.scss';
 const Slider = ({props}) => (
     <AwesomeSlider cssModule={AwsSliderStyles}>
         <div data-src="../../images/Big-Ts-Snappers-Board.jpg" />
-        <div data-src="../../images/Big-Ts-King.JPG" />
-        <div data-src="../../images/Big-Ts-Grouper.JPG" />
-        <div data-src="../../images/Big-Ts-Tuna.JPG" />
+        <div data-src="../../images/Big-Ts-King.jpg" />
+        <div data-src="../../images/Big-Ts-Grouper.jpg" />
+        <div data-src="../../images/Big-Ts-Tuna.jpg" />
         <div data-src="../../images/Big-Ts-Mahi.jpg" />
     </AwesomeSlider>
 );
