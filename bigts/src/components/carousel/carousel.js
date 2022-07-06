f38efd85db3eb35954fbc49825c2cea918759d26
Matthew <matttali09@yahoo.com>
2022-07-06 17:20:30 -0500

    adjusting image links

--- bigts/src/components/carousel/carousel.js
+++ bigts/src/components/carousel/carousel.js
@@ -3,11 +3,11 @@ import AwsSliderStyles from 'react-awesome-slider/src/core/styles.scss';
 
 const Slider = ({props}) => (
     <AwesomeSlider cssModule={AwsSliderStyles}>
-        <div data-src="./images/Big-Ts-Snappers-Board.jpg" />
-        <div data-src="./images/Big-Ts-King.JPG" />
-        <div data-src="./images/Big-Ts-Grouper.JPG" />
-        <div data-src="./images/Big-Ts-Tuna.JPG" />
-        <div data-src="./images/Big-Ts-Mahi.jpg" />
+        <div data-src="../../images/Big-Ts-Snappers-Board.jpg" />
+        <div data-src="../../images/Big-Ts-King.JPG" />
+        <div data-src="../../images/Big-Ts-Grouper.JPG" />
+        <div data-src="../../images/Big-Ts-Tuna.JPG" />
+        <div data-src="../../images/Big-Ts-Mahi.jpg" />
     </AwesomeSlider>
 );
   
